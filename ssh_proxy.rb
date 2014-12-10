5d5420c3726627f0dda35c41fc6259d17f42229e
Philip Corliss <pcorliss@50projects.com>
2014-12-10 13:21:45 -0600

    Removing cruft

--- ssh_proxy.rb
+++ ssh_proxy.rb
@@ -3,14 +3,9 @@
 require 'open3'
 
 $git_log = File.open('git.log', 'ab')
-$stdin_log = File.open('git_stdin.log', 'wb')
 
 def git_log(type, str)
   type = type.to_s.upcase
-
-  if type == 'STDIN'
-    $stdin_log.write str
-  end
 
   str.to_s.each_line do |line|
     $git_log.write type
@@ -23,57 +18,14 @@ def git_log(type, str)
     end
   end
   $git_log.flush
-
 end
 
 git_log(:cmd, ['ssh', '-x', *ARGV].join(' '))
 Open3.popen3('ssh', '-x', *ARGV) do |stdin, stdout, stderr, wait_thr|
   i = 0
-
-  pid = wait_thr.pid
-
-  until false do
-    #git_log(:iter, i)
-
+  while wait_thr.alive? && i < 100 do
     std_collection = [$stdin, stdout, stderr].reject(&:closed?)
     readers, writers, errors = IO.select(std_collection, [], [], 1)
-
-    #if readers
-      #readers.each do |reader|
-        #line = reader.readline
-        #type = [$stdin, stdout, stderr].detect { |io| io == reader }
-        #git_log(type, line)
-      #end
-    #end
-
-    #if i > 5
-      #git_log(:test, $stdin.eof?)
-    #end
-
-    #git_log(:read, readers.inspect + "\n")
-    #git_log(:proc, [wait_thr.alive?, wait_thr.class.to_s, wait_thr.inspect].join(' '))
-    #git_log(:fiha, [$stdin, $stdout, $stderr, stdin, stdout, stderr].map(&:closed?).inspect)
-
-    unless wait_thr.alive?
-      git_log(:sys, "Thread is dead #{wait_thr.value}")
-      exit
-    end
-
-    if readers.nil? && i > 5
-      #$stdin.close
-      #stdin.close
-      #
-      git_log(:sys, "Closing STDIN")
-      stdin.close
-    end
-
-    if i > 45
-      git_log(:sys, "45 iterations")
-      exit
-    end
-
-    #git_log(:writ, writers.inspect)
-    #git_log(:erro, errors.inspect)
 
     if readers && readers.include?($stdin)
       block = nil
